46bf48a973c14c94405d633cd80133c525108070
yanff <64621814@QQ.com>
2016-06-01 10:36:32 +0800

    修改颜色

--- DOPdemo/DOPDropDownMenu - Enhanced/DOPDropDownMenu.m
+++ DOPdemo/DOPDropDownMenu - Enhanced/DOPDropDownMenu.m
@@ -258,7 +258,7 @@
     for (int i = 0; i < _numOfMenu; i++) {
         //bgLayer
         CGPoint bgLayerPosition = CGPointMake((i+0.5)*bgLayerInterval, self.frame.size.height/2);
-        CALayer *bgLayer = [self createBgLayerWithColor:[UIColor whiteColor] andPosition:bgLayerPosition];
+        CALayer *bgLayer = [self createBgLayerWithColor:[UIColor colorWithRed:242/255. green:242/255. blue:242/255. alpha:1.f] andPosition:bgLayerPosition];
         [self.layer addSublayer:bgLayer];
         [tempBgLayers addObject:bgLayer];
         //title
